0b53500eb3aa00e63ae1995f808cf86f546b01b5
Esteban <esteban@geodb.com>
2020-04-17 21:48:46 +0200

    scalpingMacd strategy

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ dotenv.config();
 
 let config = {};
 
-config.pair = "BTTBNB";
+config.pair = "BTCUSDT";
 
 // Intervals: 1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
-config.interval = "1d";
+config.interval = "5m";
 
 config.minCandles = 150;
 config.candleLimit = 365 + config.minCandles + 1; //Obtiene la actual y la borramos (Velas + Aditivo + Pop)
-config.strategyName = "diaria-6-21-50";
+config.strategyName = "scalpingMacd";
 
 config.realTime = false;
 
---
+++ strategies/scalpingMacd/strategy.js
@@ -0,0 +1,78 @@
+const ema = require('./../../indicators/ema');
+const macd = require('./../../indicators/macd');
+const { generateMsg } = require('./../../node/telegramBot');
+const { buy, sell } = require('./../../node/paperTrading.js');
+const { realBuy, realSell } = require('./../../node/realTrading');
+
+//  buy(paperTrading, actualCandle.close);
+//  sell(paperTrading, actualCandle.close);
+
+module.exports = {
+
+    onRealTime: function(bot) {
+    
+    },
+
+    onCandle: function(bot) {
+        ema(bot, "ema9", 9);
+        ema(bot, "ema12", 12);
+        ema(bot, "ema26", 26);
+
+        macd(bot)
+
+        console.log(`\nTIME: ${bot.actualCandle.time}`);
+        if(bot.realTrading) generateMsg(bot.pair, bot.actualCandle.time, "Info", bot.actualCandle.close);
+
+        if(bot.enoughCandles){
+            console.log("\nActual Value: " + bot.actualCandle.close)
+            console.log("Bot status: " + bot.state)
+            console.log("MACD: " + bot.actualCandle.macd)
+            console.log("Signal: " + bot.actualCandle.signal)
+
+            let buyValueUp = bot.actualCandle.macd;
+            let buyValueDown = bot.actualCandle.signal;
+
+            let sellValueUp = bot.actualCandle.signal;
+            let sellValueDown = bot.actualCandle.macd;
+
+            let overZero = false;
+
+            // Histograma mayor que 0 y macd > signal  (1D)
+            if (!bot.lookback[0] || bot.lookback[0].ema26 == undefined){
+                return; 
+            } 
+
+            if (buyValueUp > 0 && buyValueDown > 0) overZero = true;
+
+            if(bot.state === 'initial' || bot.state === 'sell') {
+                if (!overZero){
+                    return;
+                }
+                if(buyValueUp > buyValueDown && bot.lookback[0].macd < bot.lookback[0].signal){
+                    console.log(`\nTIME: ${bot.actualCandle.time} ------ COMPRA \n`);
+                    if(bot.realTrading){
+                        generateMsg(bot.pair, bot.actualCandle.time, "preOrder", bot.actualCandle.close);
+                        realBuy(bot)
+                    } 
+                    else {
+                        bot.state = 'buy';
+                        buy(bot);
+                    }
+                }
+            }
+            else if(bot.state === 'buy' && sellValueUp > sellValueDown){
+                console.log(`\nTIME: ${bot.actualCandle.time} ------ VENTA`);
+                if(bot.realTrading){
+                    generateMsg(bot.pair, bot.actualCandle.time, "preOrder", bot.actualCandle.close);
+                    realSell(bot)
+                }
+                else {
+                    bot.state = 'sell';
+                    let benefice = sell(bot); 
+                    console.log("BENEFICIO: " + benefice + "\n");
+                }
+            }
+        }
+    } 
+}
+
